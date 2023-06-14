2817451e07a74213bcc3eac61b828c22fdd68a91
Lizeth Tancara <liztancarasirpa@gmail.com>
2023-06-13 21:30:45 -0300

    Agregando javascript en la sección contacto del proyecto

---
+++ js/contacto.js
@@ -0,0 +1,7 @@
+let enviar = document.querySelector("#enviarFormulario");
+
+enviar.addEventListener("click", function () {
+  alert(
+    "Gracias por su consulta, responderemos a la brevedad. Helen Rossatera."
+  );
+});
